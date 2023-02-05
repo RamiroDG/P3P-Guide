7ae8ca2e255f6c3b10757caf075e9bc35a6f7511
unknown <mail.ramirodg+github@gmail.com>
2023-02-05 12:35:00 -0300

    fixed css for all screen sizes

--- src/components/SLinkCard/SLinkCard.js
+++ src/components/SLinkCard/SLinkCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const SLinkCard = ({arcana, name, sprite, guide, setPerson}) => {
     return(
             <div 
-            className="card column is-1-desktop is-one-quarter-mobile" 
+            className="card column is-1-desktop is-one-quarter-mobile is-one-fifth-tablet" 
             onClick={()=>setPerson({arcana,name,guide})}
             >
                 <div className="card-image">
